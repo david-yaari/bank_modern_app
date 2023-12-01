049d5fbbad0e5b61761d38fd12eeb641086bee60
David Yaari <david,yaari@icloud.com>
2023-12-01 02:41:47 +0200

    Fixed Tailwing configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+//import React from 'react';
 
-export function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
